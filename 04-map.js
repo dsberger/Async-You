c14dd786ea3ba59205051406a1a9ddd8d2a0427e
Dan Berger <dsberger@gmail.com>
2016-07-16 18:40:35 -0700

    de-anonymizes Exercise 4

--- 04-map.js
+++ 04-map.js
@@ -3,24 +3,24 @@ var http = require('http')
 
 var urls = process.argv.slice(2)
 
-async.map(
-  urls,
-  function (url, callback) {
-    var body = ''
+async.map(urls, queryUrl, logResult)
 
-    http.get(url, function (res) {
-      res.on('data', function (chunk) {
-        body += chunk.toString()
-      })
-      res.on('end', function () {
-        return callback(null, body)
-      })
-    }).on('error', function (err) {
-      callback(err)
+function queryUrl (url, callback) {
+  var body = ''
+
+  http.get(url, function (res) {
+    res.on('data', function (chunk) {
+      body += chunk.toString()
     })
-  },
-  function (err, results) {
-    if (err) { return console.log(err) }
-    console.log(results)
-  }
-)
+    res.on('end', function () {
+      return callback(null, body)
+    })
+  }).on('error', function (err) {
+    callback(err)
+  })
+}
+
+function logResult (err, result) {
+  if (err) { return console.log(err) }
+  console.log(result)
+}
